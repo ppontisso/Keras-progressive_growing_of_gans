00db91802f09d8ce3ac7008665bdc84b53198dc1
LAPTOP-6IIBOAI5\Paul <paul.pontisso@gmail.com>
2019-05-02 21:43:45 +0200

    created function to replace batch normalization layers

--- Progressive growing of GANs/model.py
+++ Progressive growing of GANs/model.py
@@ -271,6 +271,57 @@ def Discriminator(
     model.gdrop_strength=gdrop_strength
     return model
 
+def replace_batch_norm(model):
+    # source : https://stackoverflow.com/questions/49492255/how-to-replace-or-insert-intermediate-layer-in-keras-model
+    # replace all the batch normalization layers in the model with new ones and return the twin model
+
+    # Auxiliary dictionary to describe the network graph
+    network_dict = {'input_layers_of': {}, 'new_output_tensor_of': {}}
+
+    # Set the input layers of each layer
+    for layer in model.layers:
+        for node in layer.outbound_nodes:
+            layer_name = node.outbound_layer.name
+            if layer_name not in network_dict['input_layers_of']:
+                network_dict['input_layers_of'].update(
+                    {layer_name: [layer.name]})
+            else:
+                network_dict['input_layers_of'][layer_name].append(layer.name)
+
+    # Set the output tensor of the input layer
+    network_dict['new_output_tensor_of'].update(
+        {model.layers[0].name: model.input})
+
+    # Iterate over all layers after the input
+    for layer in model.layers[1:]:
+
+        # Determine input tensors
+        layer_input = [network_dict['new_output_tensor_of'][layer_aux]
+                       for layer_aux in network_dict['input_layers_of'][layer.name]]
+        if len(layer_input) == 1:
+            layer_input = layer_input[0]
+
+        # Insert layer if name matches the regular expression
+        if 'BN' in layer.name:
+            # replace layer
+            x = layer_input
+
+            new_layer = BatchNormalization(name=layer.name + '_twin')
+
+            x = new_layer(x)
+
+        else:
+            x = layer(layer_input)
+
+        # Set new output tensor (the original one, or the one of the inserted
+        # layer)
+        network_dict['new_output_tensor_of'].update({layer.name: x})
+
+    twin = Model(inputs=model.inputs, outputs=x)
+
+    return twin
+
+
 def PG_GAN(G,D,latent_size,label_size,resolution,num_channels):
 
 
